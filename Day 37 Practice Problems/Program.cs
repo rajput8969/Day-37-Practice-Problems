47e6c5688a88ebd2c6631bb1c3389e778b913bb5
ANMOL8969 <Anmol8969_anmolsinghrajput639@gmail.com>
2022-04-22 13:26:25 +0530

    Add project files.

---
+++ Day 37 Practice Problems/Program.cs
@@ -0,0 +1,12 @@
+﻿using System;
+
+namespace Day_37_Practice_Problems
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Hello World!");
+        }
+    }
+}
